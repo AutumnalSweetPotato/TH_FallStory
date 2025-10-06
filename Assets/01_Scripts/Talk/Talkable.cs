567580c07e548859a31be4709da99a8c059ab511
AutumnalSweetPotato <2089290095x@gmail.com>
2025-10-06 19:23:56 +0800

    添加对话框功能

---
+++ Assets/01_Scripts/Talk/Talkable.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Talkable : MonoBehaviour
+{
+    [SerializeField] private bool isEntered;
+    [SerializeField] private bool hasName;
+    [TextArea(1,3)]public string[] talkLines;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isEntered = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isEntered = false;
+        }
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isEntered && !DialogueManager.Instance.dialogueBox.activeSelf)
+        {
+            DialogueManager.Instance.ShowDialogue(talkLines,hasName);
+        }
+    }
+}
---
+++ Assets/01_Scripts/UI/DialogueManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : Single<DialogueManager>
+{
+    public GameObject dialogueBox;
+    public Text dialogueText, nameText;
+
+    [TextArea(1, 3)]
+    public string[] dialogueLines;
+    [SerializeField] private int currentLine;
+    [SerializeField] private float textSpeed;
+
+    private bool isScrolling;
+    protected override void Awake()
+    {
+        base.Awake();
+        dialogueText.text = string.Empty;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && dialogueBox.activeSelf && !isScrolling)
+        {
+            currentLine++;
+
+            if (currentLine < dialogueLines.Length)
+            {
+                CheckName();
+                //dialogueText.text = dialogueLines[currentLine];
+                StartCoroutine(ScrollingText());
+            }
+            else
+            {
+                dialogueBox.SetActive(false);
+                Player.Instance.EnablePlayerInput();
+            }
+        }
+    }
+
+    public void ShowDialogue(string[] newLines,bool hasName)
+    {
+        dialogueBox.SetActive(true);
+        dialogueLines = newLines;
+        currentLine = 0;
+        CheckName();
+        //dialogueText.text = dialogueLines[currentLine];
+        StartCoroutine(ScrollingText());
+        nameText.gameObject.SetActive(hasName);
+        Player.Instance.DisablePlayerInput();
+    }
+    private void CheckName()
+    {
+        if (dialogueLines[currentLine].StartsWith("n-"))
+        {
+            nameText.text = dialogueLines[currentLine].Substring(2);
+            currentLine++;
+
+        }
+    }
+
+    private IEnumerator ScrollingText()
+    {
+        isScrolling = true;
+        dialogueText.text = "";
+        foreach(char c in dialogueLines[currentLine].ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+        isScrolling = false;
+    }
+
+}
